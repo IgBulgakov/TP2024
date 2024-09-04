6f4fadbfa5ca41f674c4ce1f656a3c920efe839e
IgBulgakov <IgBulgakov@ya.ru>
2024-09-04 15:25:52 +0300

    Лабораторная работа 1. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Stock sber = new Stock("SBER", "ПАО Сбербанк");
+
+        sber.setPreviousClosingPrice(281.50);
+
+        sber.setCurrentPrice(282.87);
+
+        System.out.println("Код: " + sber.getSymbol());
+        System.out.println("Имя: " + sber.getName());
+        System.out.println("Стоимость акций: " + sber.getCurrentPrice());
+        System.out.printf("Процент изменения: %.2f%%", sber.getChangePercent());
+    }
 }
