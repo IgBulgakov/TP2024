5f62c3a24f52cfbc65efb1fb881853ff25abbcb5
IgBulgakov <IgBulgakov@ya.ru>
2024-09-04 15:24:32 +0300

    Лабораторная работа 1. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(4, 40);
+        Rectangle rect2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("Прямоугольник 1:");
+        System.out.println("Ширина: " + rect1.getWidth());
+        System.out.println("Высота: " + rect1.getHeight());
+        System.out.println("Площадь: " + rect1.getArea());
+        System.out.println("Периметр: " + rect1.getPerimeter());
+
+        System.out.println("\nПрямоугольник 2:");
+        System.out.println("Ширина: " + rect2.getWidth());
+        System.out.println("Высота: " + rect2.getHeight());
+        System.out.println("Площадь: " + rect2.getArea());
+        System.out.println("Периметр: " + rect2.getPerimeter());
+    }
 }
